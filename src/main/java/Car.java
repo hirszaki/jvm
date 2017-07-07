ad4244be979d88eac479007018ad9cdd04c6f630
rafalpieniazek <rkpieniazek@gmail.com>
2017-07-07 19:21:18 +0200

    reflection

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,5 +1,19 @@
 public class Car {
-    static {
-        System.out.println("Loading A class");
+    Color color;
+    String brand;
+    private String owner;
+
+    public Car() {
+        color = Color.GREEN;
+        brand = "unknow";
+    }
+
+    public Car(Color color, String brand) {
+        this.color = color;
+        this.brand = brand;
+    }
+
+    public void drive() {
+        System.out.println("brum brum");
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,48 @@
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 public class Main {
 
     public static void main(String[] args) {
+
         try {
-            Class.forName("Car");
+            //Aspect Oriented Programming
+
+            System.out.println("Methods:");
+            Class<? extends Object> aClass = Class.forName("Car");
+            Method[] methods = aClass.getDeclaredMethods();
+            for (Method method : methods) {
+                System.out.println(method);
+            }
+
+            System.out.println("Fields:");
+            Field[] fields = aClass.getDeclaredFields();
+            for (Field field : fields) {
+                System.out.println(field);
+            }
+
+            System.out.println("Constructor:");
+            Constructor<?>[] constructors = aClass.getConstructors();
+            for (Constructor<?> constructor : constructors) {
+                System.out.println(constructor);
+            }
+
+            System.out.println(aClass.getClassLoader());
+
+            Car car  = (Car) aClass.newInstance();
+
+
+            System.out.println(car.color);
+            System.out.println(car.brand);
+            car.drive();
+
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
         }
-        System.out.println(Car.class.getClassLoader());
     }
 }
